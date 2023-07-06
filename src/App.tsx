f5bad3c9458f63e057495ba46c764d4f9ae8b185
loipv03 <phanvanloi1522003@gmail.com>
2023-07-07 06:00:55 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import { Home } from "./import";
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.tsx'
-import './index.scss'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
